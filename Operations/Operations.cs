e6b5f2ff1cddf5b518644fe423a7eba128e75b48
JlaHDblLLl <manex.gamestrtrpo@gmail.com>
2022-09-07 20:41:31 +0300

    Adding operations of List

---
+++ Operations/Operations.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    public class Operations
+    {
+        public Operations(List<double> listik)
+        {
+            Listik = listik;
+        }
+
+        List<double> Listik { get; set; }
+
+        public List<double> Up(string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    Listik[0] = Listik[1];
+                    Listik[1] = Listik[2];
+                    Listik[2] = Listik[3];
+                    Listik[3] += Listik[1];
+                    break;
+                case "-":
+                    Listik[0] = Listik[1];
+                    Listik[1] = Listik[2];
+                    Listik[2] = Listik[3];
+                    Listik[3] -= Listik[1];
+                    break;
+                case "/":
+                    Listik[0] = Listik[1];
+                    Listik[1] = Listik[2];
+                    Listik[2] = Listik[3];
+                    Listik[3] /= Listik[1];
+                    break;
+                case "*":
+                    Listik[0] = Listik[1];
+                    Listik[1] = Listik[2];
+                    Listik[2] = Listik[3];
+                    Listik[3] *= Listik[1];
+                    break;
+                default:
+                    Listik[0] = Listik[1];
+                    Listik[1] = Listik[2];
+                    Listik[2] = Listik[3];
+                    Listik[3] = 0d;
+                    break;
+            }
+            return Listik;
+        }
+        public List<double> Down()
+        {
+            
+            Listik[3] = Listik[2];
+            Listik[2] = Listik[1];
+            Listik[1] = Listik[0];
+            return Listik;
+        }
+        public List<double> Switch()
+        {
+            double buff = Listik[3];
+            Listik[3] = Listik[2];
+            Listik[2] = buff;
+            return Listik;
+        }
+        public List<double> Turn()
+        {
+            double buff = Listik[0];
+            Listik[0] = Listik[1];
+            Listik[1] = Listik[2];
+            Listik[2] = Listik[3];
+            Listik[3] = buff;
+            return Listik;
+        }
+    }
+}
